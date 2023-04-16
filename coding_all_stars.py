003d789f6eee65c518c7371e1eeef2ab4e5fa521
IsabelAngel <lisabel.angell@gmail.com>
2023-04-15 23:19:00 -0500

    Update coding_all_stars.py code. The language detector was implemented with web scrapping and the images are listed in a variable.

--- coding_all_stars.py
+++ coding_all_stars.py
@@ -4,4 +4,48 @@ Created on Sat Apr 15 20:05:45 2023
 
 @author: ASUS
 """
+from bs4 import BeautifulSoup
+import pandas as pd
+from urllib.request import urlopen
+from langdetect import detect, DetectorFactory
+from urllib.error import HTTPError, URLError
+from urllib3.exceptions import SSLError
+import ssl 
 
+def func(string):
+    DetectorFactory.seed = 0
+    language = detect(" ".join(' '.join(string.splitlines()).split()))  
+    return language
+
+
+df = pd.read_csv('Trial Task Sample URLs.csv', on_bad_lines='skip', sep= ';')
+
+iterador = 0
+for item in df.URL.values.tolist():
+    try:
+        print(item)
+        url = df.URL.values.tolist()[iterador]
+        page = urlopen(url)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        texto = func(soup.get_text())
+        print(texto)
+    except HTTPError:
+        print("Página Inexistente")
+        continue
+    except ValueError:
+        print("Unknown url type: " + item)
+    except SSLError:
+        print("Certificate has expired: " + item)
+    except URLError:
+        print("Certificate verify failed: certificate has expired: " + item)
+    finally:
+        iterador += 1
+
+imagenes = []
+for image in soup.find_all("img"):
+    imagenes.append(image)
+    
+# imagenes = soup.find_all("img")
+
+# lista_url = df.URL.values.tolist()
